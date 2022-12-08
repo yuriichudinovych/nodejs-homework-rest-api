5af4e64c2b65753fc85880a06b6cf23bc1f76728
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-12-08 20:33:48 +0200

    init database

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+// 8fyEtd5HUYMgc8Oy
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// const dotenv = require("dotenv");
+// dotenv.config();
+const { DB_HOST, PORT = 3000 } = process.env;
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
