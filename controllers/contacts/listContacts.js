142aba7b299eb6f5f7b2c57ed5efd1f23efdbad0
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-12-17 18:52:44 +0200

    added filter by favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,9 +1,11 @@
+const { any, object, boolean } = require("joi");
 const { Contact } = require("../../models");
 const listContacts = async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite = [false, true] } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id }, "", {
+
+  const contacts = await Contact.find({ owner: _id, favorite }, "", {
     skip,
     limit: Number(limit),
   }).populate("owner", "_id email subscription");
